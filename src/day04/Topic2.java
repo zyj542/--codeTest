89ca1bcc845e394a64f5e74de05b8c2f0114ab08
zyj <zyj542968@163.com>
2022-07-09 12:13:15 +0800

    第四天作业-大数_时间_文件流-改正

--- src/day04/Topic2.java
+++ src/day04/Topic2.java
@@ -3,47 +3,52 @@ package day04;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
-/*  1 使用大数运算，就算
-    4444444.4444/3.333333的值，保留2位小数，四舍五入
+/*
     2  输入你生日的前一周的周四是该年的第几天？
     Calendar:set方法设置到你的生日，
     add方法跳到前一周的周四，通过 xxxx获得是该年的第几天? get(Calendar.DAY_OF_YEAR)
-    3、递归输出src目录中的所有.java文件的绝对路径。
-    4、在电脑D盘下创建一个文件为HelloWorld.txt文件，判断他是文件还是目录，再创建一个目录IOTest，之后将HelloWorld.txt拷贝到IOTest目录的a.txt去。
-    5、从utf-8编码格式的文本复制到GBK编码的文本（不能出现乱码）*/
+    */
 public class Topic2 {
     //    输入你生日的前一周的周四是该年的第几天？2000年05月21日
-    static String[] weeks = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
 
     public static void main(String[] args) throws ParseException {
-//        Date date = new Date();
+//        周四晚上的想法
+        first();
+//听课改正后的想法
+        second();
+    }
 
+    private static void second() {
+        Calendar instance = Calendar.getInstance();
+        instance.set(2022,0,11);
+        instance.add(Calendar.DAY_OF_WEEK, -4);
+        int day = instance.get(Calendar.DAY_OF_YEAR);
+        System.out.println("我生日的前一周的周四是该年的第"+day+"天");
+    }
+
+    private static void first() throws ParseException {
+        String[] weeks = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
+//        格式化日期 ;
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Date date = dateFormat.parse("2000-05-21");
         Date date0 = dateFormat.parse("2000-01-01");
-
+//生日当天和上周四的天数距离
         SimpleDateFormat weekType = new SimpleDateFormat("E");
         String week = weekType.format(date);
-
-        int weekDiffer = differ(week);
-        System.out.println(weekDiffer);
-//        System.out.println(date.getTime()-date0.getTime()-Differ);
-        long dayDiffer=(date.getTime()-date0.getTime())/(3600*24*1000)-weekDiffer;
-
-        System.out.println(dayDiffer);
-
-    }
-
-    static int differ(String week) {
+        int weekDiffer=0;
         for (int i = 0; i < weeks.length; i++) {
             if (weeks[i].equals(week)) {
-                System.out.println("i"+i);
-                return i+7-4;
+                 weekDiffer = i+7-4;
             }
         }
-        return -1;
+//        生日和当年一月一号的的天数距离减去生日当天和上周四的天数距离
+        long dayDiffer=(date.getTime()-date0.getTime())/(3600*24*1000)-weekDiffer;
+
+        System.out.println("我生日的前一周的周四是该年的第"+dayDiffer+"天");
     }
+
 
 }
