d925d52fb66e0976616906208baeeaffe339b26d
zyj <zyj542968@163.com>
2022-07-11 00:18:17 +0800

    第五天作业-[读取\写入文件,冒泡算法\重写比较函数compareTo]

--- src/day05/Topic02.java
+++ src/day05/Topic02.java
@@ -1,7 +1,7 @@
 package day05;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 /*2 创建类 Student：
         int sid(学号)
@@ -9,10 +9,11 @@ import java.util.Scanner;
         int age;
 要求使用两种方法实现自定义比较规则： 先按学号排，如果学号学号重复，按年龄排。
 并遍历输出结果*/
-class Student {
+class Student implements Comparable<Student> {
     private final int sid;
     private final int age;
     private final String name;
+
 
     public Student(int sid, int age, String name) {
         this.sid = sid;
@@ -40,48 +41,65 @@ class Student {
                 ", name='" + name + '\'' +
                 '}';
     }
+
+    @Override
+    public int compareTo(Student o) {
+        if (this.getSid() == o.getSid()) {
+            return this.getAge() - o.getAge();
+        } else return Integer.compare(this.getSid(), o.getSid());
+    }
 }
 
 public class Topic02 {
     public static void main(String[] args) {
 //        Scanner sc = new Scanner(System.in);
 //        Student[] stuArr = setStudents(sc);//键盘输入的方法
-        Student[] stuArr = new Student[3];
-        stuArr[0] = new Student(193, 20, "张三");
+        Student[] stuArr = new Student[6];
+        stuArr[0] = new Student(190, 20, "张三");
         stuArr[1] = new Student(193, 18, "李四");
         stuArr[2] = new Student(192, 30, "王五");
-
-        planA(stuArr);
-        planB(stuArr);
+        stuArr[3] = new Student(191, 16, "赵六");
+        stuArr[4] = new Student(191, 88, "孙七");
+        stuArr[5] = new Student(1, 66, "周八");
+        Student[] clone = stuArr.clone();
+        planA(stuArr);//从大到小排序,冒泡算法
+        System.out.println("--------------------------");
+        planB(clone);//   重写compareTo,从小到大排序
     }
 
     //    从大到小 排序冒泡
     private static void planA(Student[] stuArr) {
-        for (int i = 0; i < stuArr.length - 1; i++) {
+        for (int i = 0; i < stuArr.length; i++) {
 
-            Student stuTemp = new Student(0, 0, null);
-            for (int j = i; j < stuArr.length - 1; j++) {
+            Student stuTempMax = new Student(0, 0, null);
+            for (int j = i; j < stuArr.length; j++) {
 
-                if (stuTemp.getSid() > stuArr[i].getSid()) {
-                    stuTemp = stuArr[j];
+                if (stuTempMax.getSid() < stuArr[j].getSid()) {
+                    stuTempMax = stuArr[j];
                     stuArr[j] = stuArr[i];
-                    stuArr[i] = stuTemp;
-                } else if (stuTemp.getSid() == stuArr[i].getSid() && stuTemp.getAge() > stuArr[i].getAge()) {
-                    stuTemp = stuArr[j];
+                    stuArr[i] = stuTempMax;
+                } else if (stuTempMax.getSid() == stuArr[j].getSid() && stuTempMax.getAge() < stuArr[j].getAge()) {
+                    stuTempMax = stuArr[j];
                     stuArr[j] = stuArr[i];
-                    stuArr[i] = stuTemp;
+                    stuArr[i] = stuTempMax;
                 }
             }
         }
         for (Student s:stuArr
              ) {
-            System.out.println(s);
+            System.out.println("学号:"+s.getSid()+"\t\t年龄:"+s.getAge()+"\t\t名字:"+s.getName());
 
         }
     }
 
-    //    从小到大排序 插入排序
+    //   重写compareTo,从小到大排序
     private static void planB(Student[] stuArr) {
+        ArrayList<Student> students = new ArrayList<>(Arrays.asList(stuArr));
+        Collections.sort(students);
+        for (Student s : students
+        ) {
+            System.out.println("学号:"+s.getSid()+"\t\t年龄:"+s.getAge()+"\t\t名字:"+s.getName());
+        }
 
     }
 
@@ -103,6 +121,7 @@ public class Topic02 {
         }
         return stuArr;
     }
+
 
 
 }
--- src/day05/Topic03.java
+++ src/day05/Topic03.java
@@ -1,8 +1,27 @@
 package day05;
-        /*3  【提高】创建类 Pet: String name(宠物的名字) ,int feetCount（脚的个数）
-        设计类Family：String owner（主人），此外，每个家庭可以养有多只宠物（使用什么样的集合去存储宠物？？）。
-        a 创建2个family对象，分别拥有2只和3只宠物
-        b 写静态方法  ：   public static switchPets(Family f1,Famliy f2):  将两个家庭 脚最多的宠物进行互换。并输出互换后的结果。
-        特别注意：如果有多只宠物的脚条数一样，任选一只进行调换即可。*/
+
+import java.util.ArrayList;
+
+/*3  【提高】创建类 Pet: String name(宠物的名字) ,int feetCount（脚的个数）
+设计类Family：String owner（主人），此外，每个家庭可以养有多只宠物（使用什么样的集合去存储宠物？？）。
+a 创建2个family对象，分别拥有2只和3只宠物
+b 写静态方法  ：   public static switchPets(Family f1,Famliy f2):  将两个家庭 脚最多的宠物进行互换。并输出互换后的结果。
+特别注意：如果有多只宠物的脚条数一样，任选一只进行调换即可。*/
+class Pet {
+    private String name;
+    private int feetCount;
+    private String owner;
+}
+
+class Family {
+    private String owner;
+}
 public class Topic03 {
+    public static void main(String[] args) {
+        test1();
+    }
+
+    private static void test1() {
+
+    }
 }
